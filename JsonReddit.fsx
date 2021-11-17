356b976ecc64986ff568b9ae4478874819912358
Brayan Lopez <brlo@sent.com>
2021-11-17 17:09:28 -0500

    Added a few more JSON handling scripts

---
+++ JsonReddit.fsx
@@ -0,0 +1,18 @@
+﻿#r "nuget: FSharp.Data"
+open System
+open FSharp.Data
+
+[<Literal>]
+let subredditFormat = "https://www.reddit.com/r/all.json"
+type Subreddit = JsonProvider<subredditFormat>
+
+let GetSubredditJson sub =
+    Subreddit.Load($"https://www.reddit.com/r/{sub}.json")
+
+let GetLatestPostTitles sub =
+    let currentSub = GetSubredditJson sub
+    currentSub.Data.Children |> Array.map (fun x -> x.Data.Title)
+    
+let GetYouTubeLinks sub =
+    let currentSub = GetSubredditJson sub
+    currentSub.Data.Children |> Array.filter (fun x -> x.Data.Url.Contains("youtube.com")) |> Array.map (fun x -> (x.Data.Title, x.Data.Url))
