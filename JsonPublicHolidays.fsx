4fa1357fae0f0b5374923bb88b1f55e9f6e358b8
Brayan Lopez <brlo@sent.com>
2021-11-18 01:02:58 -0500

    a little more for JsonPublicHolidays.fsx

--- JsonPublicHolidays.fsx
+++ JsonPublicHolidays.fsx
@@ -11,9 +11,16 @@ let GetHolidayList country year =
     GetPublicHolidays country year
     |> Array.map (fun x -> (x.LocalName, x.Date.ToShortDateString()))
     
+let GetHolidaysInMonth country year month =
+    GetPublicHolidays country year
+    |> Array.filter (fun x -> x.Date.Month = month)
+    |> Array.map (fun x -> x.LocalName)
+    
 let us2021Holidays = GetHolidayList "US" 2021
     
 let gt2021Holidays = GetHolidayList "GT" 2021
     
 let ca2021Holidays = GetHolidayList "CA" 2021
- 
+
+let us2021JanuaryHolidays = GetHolidaysInMonth "US" 2021 1
+let us2021DecemberHolidays = GetHolidaysInMonth "US" 2021 12
