69eca77b7d9fb2ea21bd4c18ee84806e7ce1d83d
Brayan Lopez <brlo@sent.com>
2021-11-18 00:47:26 -0500

    a few more json handling scripts

---
+++ JsonPublicHolidays.fsx
@@ -0,0 +1,13 @@
+﻿#r "nuget: FSharp.Data"
+open FSharp.Data
+
+let [<Literal>] holidaysJson = "https://date.nager.at/api/v2/publicholidays/2021/US"
+type PublicHolidays = JsonProvider<holidaysJson>
+
+let GetPublicHolidays countryCode year =
+    PublicHolidays.Load($"https://date.nager.at/api/v2/publicholidays/{year}/{countryCode}")
+    
+let us2021Holidays =
+    GetPublicHolidays "US" 2021
+    |> Array.map (fun x -> (x.LocalName, x.Date.ToShortDateString()))
+
