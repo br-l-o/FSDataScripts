b77f7126f06437f3a8891c3ae5efb0d9fa7c46ff
Brayan Lopez <brlo@sent.com>
2021-11-18 16:32:18 -0500

    add more to wc

--- Wc.fsx
+++ Wc.fsx
@@ -3,6 +3,8 @@ open System.IO
 open System.Text.RegularExpressions
 open XPlot.Plotly
 
+fsi.ShowDeclarationValues <- false
+
 let countWords (contents: string) =
     let regex = Regex("[^a-zA-Z0-9 -]")
     regex.Replace(contents, "").Split()
@@ -13,16 +15,22 @@ let getMostCommonWords limit (contents: string) =
     |> countWords
     |> Array.sortByDescending (snd)
     |> Seq.truncate limit
-     
+    
+let getMostUncommonWords limit (contents: string) =
+    contents
+    |> countWords
+    |> Array.sortBy (snd)
+    |> Seq.truncate limit
+    
+let groupCounts (contents: string) =
+    contents
+    |> countWords
+    |> Array.groupBy (fun x -> snd x)
+    |> Array.sortByDescending (fun x -> snd x |> Array.length)
+    |> Array.map (fun x -> $"Words with {fst x} appearances", snd x |> Array.length)
+    
 // test!
 let iliad = __SOURCE_DIRECTORY__ + @"/data/iliad.txt" |> File.ReadAllText
 let iliadTop10Words = iliad |> getMostCommonWords 10
 
-let titleLayout = Layout(title = $"most common words in the Iliad")
-
-iliadTop10Words
-|> Chart.Bar
-|> Chart.WithLayout titleLayout
-|> Chart.WithHeight 500
-|> Chart.WithWidth 700
-|> Chart.Show
+let titleLayout = Layout(title = $"Occurences")
