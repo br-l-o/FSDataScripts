9be15b4309b54c8870f9d4757b23bee2ccb03f39
Brayan Lopez <brlo@sent.com>
2021-11-19 12:41:05 -0500

    more mediawiki tweaks

--- MediaWikiAPI.fsx
+++ MediaWikiAPI.fsx
@@ -13,19 +13,20 @@ type MediaWikiContents =
     { Id: int
       Title: string
       LatestTimestamp: DateTimeOffset
-      License: string }
+      License: (string * string) }
 
 let WikipediaSearchResponse (languageCode: string) (query: string) =
     MediaWikiResponse.Load($"https://{languageCode}.wikipedia.org/w/rest.php/v1/page/{query}")
 
+let WikipediaSearchResponseContents languageCode query =
+    let response =
+        WikipediaSearchResponse languageCode query
+
+    { Id = response.Id
+      Title = response.Title
+      LatestTimestamp = response.Latest.Timestamp
+      License = (response.License.Title, response.License.Url) }
+
 let EnglishWikipediaSearchResponse (query: string) = WikipediaSearchResponse "en" query
-
-let englishSearchDetails =
-    let englishSearch = EnglishWikipediaSearchResponse "Google"
-
-    { Id = englishSearch.Id
-      Title = englishSearch.Title
-      LatestTimestamp = englishSearch.Latest.Timestamp
-      License = englishSearch.License.Title }
 
 let spanishSearch = WikipediaSearchResponse "es" "Google"
