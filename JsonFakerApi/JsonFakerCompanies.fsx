784307d645ddbf6a55db7202a505b7738e7c177c
Brayan Lopez <brlo@sent.com>
2021-11-19 17:58:20 -0500

    a little formatting

--- JsonFakerApi/JsonFakerCompanies.fsx
+++ JsonFakerApi/JsonFakerCompanies.fsx
@@ -3,21 +3,21 @@ open FSharp.Data
 
 fsi.ShowDeclarationValues <- false
 
-let [<Literal>] companiesTemplate = "https://fakerapi.it/api/v1/companies?_quantity=1"
+[<Literal>]
+let companiesTemplate =
+    "https://fakerapi.it/api/v1/companies?_quantity=1"
+
 type Companies = JsonProvider<companiesTemplate>
 
-let GetCompanies (quantity: int) = 
+let GetCompanies (quantity: int) =
     try
-        Ok (Companies.Load($"https://fakerapi.it/api/v1/companies?_quantity={quantity}"))
+        Ok(Companies.Load($"https://fakerapi.it/api/v1/companies?_quantity={quantity}"))
     with
-        | _ -> Error "Error retrieving addresses. Try again later."
+    | _ -> Error "Error retrieving addresses. Try again later."
 
 let testCompanies = GetCompanies(10)
 
-let companyNames = 
+let companyNames =
     match testCompanies with
     | Error x -> Error "No companies to process."
-    | Ok y ->
-        Ok (
-            y.Data |> Array.map (fun x -> x.Name)
-        )
+    | Ok y -> Ok(y.Data |> Array.map (fun x -> x.Name))
