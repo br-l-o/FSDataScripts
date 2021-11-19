fb6d717acfbee03edc61d6881293d157c3cc0345
Brayan Lopez <brlo@sent.com>
2021-11-19 12:32:17 -0500

    mediawiki rest script

---
+++ MediaWikiAPI.fsx
@@ -0,0 +1,31 @@
+﻿open System
+
+#r "nuget: FSharp.Data"
+open FSharp.Data
+
+[<Literal>]
+let mediaWikiBase =
+    "https://en.wikipedia.org/w/rest.php/v1/page/Earth"
+
+type MediaWikiResponse = JsonProvider<mediaWikiBase>
+
+type MediaWikiContents =
+    { Id: int
+      Title: string
+      LatestTimestamp: DateTimeOffset
+      License: string }
+
+let WikipediaSearchResponse (languageCode: string) (query: string) =
+    MediaWikiResponse.Load($"https://{languageCode}.wikipedia.org/w/rest.php/v1/page/{query}")
+
+let EnglishWikipediaSearchResponse (query: string) = WikipediaSearchResponse "en" query
+
+let englishSearchDetails =
+    let englishSearch = EnglishWikipediaSearchResponse "Google"
+
+    { Id = englishSearch.Id
+      Title = englishSearch.Title
+      LatestTimestamp = englishSearch.Latest.Timestamp
+      License = englishSearch.License.Title }
+
+let spanishSearch = WikipediaSearchResponse "es" "Google"
