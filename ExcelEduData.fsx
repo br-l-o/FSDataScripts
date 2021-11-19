eb8a1155f7009025955be7037996f7b62a37332f
Brayan Lopez <brlo@sent.com>
2021-11-19 11:03:03 -0500

    text excel provider

---
+++ ExcelEduData.fsx
@@ -0,0 +1,12 @@
+﻿#r "nuget: FSharp.Data"
+#r "nuget: ExcelProvider"
+open FSharp.Data
+open FSharp.Interop.Excel
+
+// the file in this repo is null; you'll have to fetch the file yourself
+// https://data.ed.gov/dataset/college-scorecard-all-data-files-through-6-2020/resources?resource=823ac095-bdfc-41b0-b508-4e8fc3110082
+[<Literal>]
+let collegeScoreCardPath = __SOURCE_DIRECTORY__ + @"/data/Most-Recent-Cohorts-All-Data-Elements.csv"
+
+type CollegeScoreCard = ExcelFile<collegeScoreCardPath>
+let cscFile = new CollegeScoreCard()
